1f440fa1e9ce3444e1b9bba51377e2ac7127603c
Maverick0008 <93142563+Maverick0008@users.noreply.github.com>
2022-02-17 12:19:45 +0300

    сделал дз по реализции отметки и очистку всех задач

---
+++ src/tutorial-3/components/AddField.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { TextField, Button, Checkbox } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+export const AddField = ({ onAddTask }) => {
+  const [task, setTask] = useState("");
+  const [checked, setChecked] = useState(false);
+  const toggleAddTask = () => {
+    if (checked) {
+      setChecked(!checked);
+    }
+    task.trim() && onAddTask(task, checked);
+    setTask("");
+  };
+  return (
+    <div className="field">
+      <Checkbox
+        checked={checked}
+        onClick={(event) => setChecked(event.target.checked)}
+        className="checkbox"
+        icon={<RadioButtonUncheckedIcon />}
+        checkedIcon={<CheckCircleIcon />}
+      />
+      <TextField
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
+        placeholder="Введите текст задачи..."
+        variant="standard"
+        fullWidth
+      />
+      <Button onClick={toggleAddTask}>
+        <AddIcon />
+      </Button>
+    </div>
+  );
+};
+
